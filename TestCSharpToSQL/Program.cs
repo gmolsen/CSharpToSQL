e5c5174297ac1ab11babd33f520c33484b6545e6
Greg <gmolsenvideo@gmail.com>
2017-09-13 09:49:33 -0400

    comments added

--- TestCSharpToSQL/Program.cs
+++ TestCSharpToSQL/Program.cs
@@ -10,6 +10,7 @@ namespace TestCSharpToSQL {
 	class Program {
 		void Run () {
 
+			// making connection with SQL database
 			string connStr = "Server=STUDENT05;Database=DotNetDatabase;Trusted_Connection=yes";
 			SqlConnection connection = new SqlConnection(connStr);
 			connection.Open();
@@ -17,13 +18,20 @@ namespace TestCSharpToSQL {
 				Console.WriteLine("SQL connection did not open.");
 				return;
 			}
+
 			Console.WriteLine("SQL connection opened successfully.");
+			
+			//telling SQL what data we want to see
 			var sql = "select * from Student";
 			SqlCommand cmd = new SqlCommand(sql, connection);
+			
+			//data from SQL is read using SQLDataReader method "ExecuteReader"
 			SqlDataReader reader = cmd.ExecuteReader();
 			List<Student> students = new List<Student>();
 			while(reader.Read()) {
-				// GetOrdinal finds string regardless of what column it is in
+
+				// reader holds all data from database, GetOrdinal finds string regardless of what column it is in
+				// loop persists until there is no more data to read, data from SQL is assigned to local variables 
 				var id = reader.GetInt32(reader.GetOrdinal("Id"));
 				var firstName = reader.GetString(reader.GetOrdinal("FirstName"));
 				var lastName = reader.GetString(reader.GetOrdinal("LastName"));
@@ -37,14 +45,16 @@ namespace TestCSharpToSQL {
 
 				//set major id to null value before reading the database value
 				var majorId = 0;
+				
 				//check the value in the database
 				//if it is NOT NULL
 				if (!reader.GetValue(reader.GetOrdinal("MajorId")).Equals(DBNull.Value)) {
+					
 					//then do this
 					majorId = reader.GetInt32(reader.GetOrdinal("MajorId"));
 							}
 				
-
+				//local variables are used to fill this instance of "student" with data
 				Console.WriteLine($"{id}, {firstName} {lastName}, lives on {address} in {city}, {state} {zip} and was born on {birthday};");
 				Student student = new Student();
 				student.Id = id;
